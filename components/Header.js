3346f295726c0ebcd5ad5790ac608efd6c4a7476
WildHashGhost_Cica <wildhashghost@gmail.com>
2021-08-10 17:21:31 +0200

    Header section

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Image from "next/image";
-import logo from "../Images/whg.png"
+import logo from "../Images/whg.png";
+import { GlobeAltIcon, MenuIcon, SearchIcon, UserCircleIcon, } from "@heroicons/react/solid"
 
 
 function Header() {
     return (
-        <header className="sticky top-0 z-50 grid grid-cols-3 bg-black text-blue-700 shadow-md p-5 md:px-10">
+        <header className="sticky top-0 z-50 grid grid-cols-3 bg-black text-blue-400 shadow-md p-5 md:px-10">
             {/*Left*/}
-            <div className="relative flex items-center h-10 cursor-pointer my-auto">
+            <div className="relative flex items-center h-10 cursor-pointer my-auto ">
                 
                 <Image
                 src={logo}
@@ -15,16 +16,23 @@ function Header() {
                 objectFit="contain"
                 objectPosition="left"
                 /> 
-            <p style={{paddingLeft:"50px"}}><b>airbnb</b></p>
+            <p style={{paddingLeft:"50px", fontSize:"30px"}}><b>airbnb</b></p>
             </div>
             
             {/*Midle - Search*/}
-            <div>
-                <input type="text"/>
+            <div className="flex items-center md:border-2 border-blue-400 rounded-full py-2 bg-black">
+                <input type="text" placeholder="Searching for" className="flex-grow pl-5 bg-transparent outline-none placeholder-white"/>
+                <SearchIcon className=" hidden md:inline-flex h-8 pr-3 bg-white-400 text-blue rounded-full p-1 cursor-pointer"/>
             </div>
             {/*Right*/}
-            <div>
+            <div className="flex items-center space-x-4 justify-end">
+                <p className="hidden md:inline cursor-pointer">Become a host</p>
+                <GlobeAltIcon className="h-6"/>
 
+                <div className="flex items-center border-2 border-blue-400 text-white  rounded-full cursor-pointer p-2">
+                    <MenuIcon className="h-6"/>
+                    <UserCircleIcon className="h-6" />
+                </div>
             </div>
         
         </header>
