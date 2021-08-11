3b0e0adb3f9243e70c54c8d54f1503e5441e5d32
WildHashGhost_Cica <wildhashghost@gmail.com>
2021-08-11 18:41:23 +0200

    MediumCard section, scrollbar-hide added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import MediumCard from '../components/MediumCard';
 import SmallCard from '../components/SmallCard';
 
 
 
-export default function Home({exploreData}) {
+export default function Home({exploreData, cardsData}) {
 
   return (
     <div className="bg-black">
@@ -16,7 +17,7 @@ export default function Home({exploreData}) {
 
       <Header/>
       <Banner/>
-      <main className="max-w-7xl mx-auto px-8 sm:px-16 bg-black text-blue-400 border-2 border-blue-400">
+      <main className="max-w-7xl mx-auto px-8 mt-10 sm:px-16 bg-black text-blue-400 border-2 border-blue-400">
         <section className="pt-5">
           <h2 className="text-4xl font-semibold pb-5 ">Explore Nearby</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"> 
@@ -27,6 +28,18 @@ export default function Home({exploreData}) {
             img={img}
             distance={distance}
             location={location}
+            />
+          ))}
+          </div>
+        </section>
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3">
+          {cardsData?.map(({img, title}) =>(
+            <MediumCard
+            key={img}
+            img={img}
+            title={title}
             />
           ))}
           </div>
@@ -42,9 +55,14 @@ export async function getStaticProps() {
     (res) => res.json()
   );
 
+  const cardsData = await fetch('https://links.papareact.com/zp1').then(
+    (res) => res.json()
+  );
+
   return{
     props: {
-      exploreData
+      exploreData,
+      cardsData
     }
   }
 }
